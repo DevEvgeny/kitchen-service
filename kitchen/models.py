63d9fa83a0435535ceecd8678a53519cdb2bac84
Polevoy Yevhen <polevoye90@gmail.ua>
2025-06-23 12:45:27 +0300

    refactor model Dish, add fields max_digits and decimal_places,add AUTH_USER_MODEL = kitchen.Cook in settings

--- kitchen/models.py
+++ kitchen/models.py
@@ -22,9 +22,9 @@ class Cook(AbstractUser):
 class Dish(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
-    cooks = models.ManyToManyField(Cook, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
 
     class Meta:
         ordering = ["name"]
