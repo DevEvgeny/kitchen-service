253bfb07400d5e31de56dc4a7b365e51c6be2899
Polevoy Yevhen <polevoye90@gmail.ua>
2025-06-23 15:10:47 +0300

    add admin.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from kitchen.models import Cook, Dish, DishType
+
+
+# @admin.register(Cook)
+# class CookAdmin(UserAdmin):
+#     list_display = UserAdmin.list_display + ("year_of_experience", )
+#     fieldsets = UserAdmin.fieldsets + (
+#         (("Personal info", {"fields": ("year_of_experience",)}),)
+#     )
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    fieldsets = list(UserAdmin.fieldsets)
+    list_display = UserAdmin.list_display + ("year_of_experience",)
+
+    for i, (name, section) in enumerate(fieldsets):
+        if name == "Personal info":
+            section["fields"] = section["fields"] + ("year_of_experience",)
+            fieldsets[i] = (name, section)
+            break
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ["name",
+                    "price",
+                    "dish_type",
+                    ]
+
+
+admin.site.unregister(Group)
+admin.site.register(DishType)
--- kitchen/models.py
+++ kitchen/models.py
@@ -13,7 +13,7 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    year_of_experience = models.IntegerField()
+    year_of_experience = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.username} {self.first_name} {self.last_name}"
@@ -21,7 +21,7 @@ class Cook(AbstractUser):
 
 class Dish(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
     cooks = models.ManyToManyField(Cook, related_name="dishes")
@@ -30,4 +30,4 @@ class Dish(models.Model):
         ordering = ["name"]
 
     def __str__(self):
-        return f"{self.name} {self.dish_type} {self.price} {self.cooks}"
+        return f"{self.name} {self.dish_type} {self.price}"
