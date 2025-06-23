b1dd3587aab119eb158743bc55156d7baa10f3fc
Polevoy Yevhen <polevoye90@gmail.ua>
2025-06-23 12:28:59 +0300

    create model DishType Cook Dish

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering =["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    year_of_experience = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.username} {self.first_name} {self.last_name}"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField()
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(Cook, on_delete=models.CASCADE, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} {self.dish_type} {self.price} {self.cooks}"
