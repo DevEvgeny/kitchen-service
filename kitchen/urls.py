4e882675acd7d28bc2a1a2a7562341de0ece7df4
Polevoy Yevhen <polevoye90@gmail.ua>
2025-06-24 13:47:52 +0300

    add dishes list with url,view and html

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from .views import (index,
-                    DishTypeListView)
+                    DishTypeListView,
+                    DishListView)
 
 
 urlpatterns = [
     path("", index, name="index"),
-    path("dish-types/", DishTypeListView.as_view(), name="dish-types-list")
+    path("dish-types/", DishTypeListView.as_view(), name="dish-types-list"),
+    path("dishes/", DishListView.as_view(), name="dish-list")
 ]
 
 app_name = "kitchen"
