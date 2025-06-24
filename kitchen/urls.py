954a416b12382d71beb565a415160a8c0a2c24ae
Polevoy Yevhen <polevoye90@gmail.ua>
2025-06-24 13:08:07 +0300

    add static/css/styles.css, add view DishTypeListView

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import index
+from .views import (index,
+                    DishTypeListView)
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-types-list")
 ]
 
 app_name = "kitchen"
